1252d45f88021180be29a33a8ad433efc429f813
Sioxox <sioxox@pm.me>
2023-01-02 05:49:09 +0100

    First usable visual flow-based editor using flow_graph

--- lib/main.dart
+++ lib/main.dart
@@ -1,138 +1,171 @@
+import 'package:flow_graph/flow_graph.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: FlowEditor(),
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
     );
   }
 }
 
-class Node {
-  double x;
-  double y;
-  int id;
-  String label;
-  List<Connector> inputs = [];
-  List<Connector> outputs = [];
-  Offset canvasOffset;
-  void Function(double dx, double dy) updatePosition;
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
 
-  Node({
-    required this.id,
-    this.x = 0,
-    this.y = 0,
-    this.label = 'Node',
-    required this.canvasOffset,
-    required this.updatePosition,
-  });
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
 
-  Widget buildWidget(BuildContext context) {
-    return Positioned(
-      left: x + canvasOffset.dx,
-      top: y + canvasOffset.dy,
-      child: GestureDetector(
-        onPanUpdate: (details) {
-          updatePosition(details.delta.dx, details.delta.dy);
-        },
-        child: Container(
-          width: 200,
-          height: 100,
-          color: Colors.white,
-          child: Column(
-            children: [
-              Text('Node $id'),
-              ...inputs.map((input) => input.buildWidget(context)),
-              ...outputs.map((output) => output.buildWidget(context)),
-            ],
-          ),
+class _MyHomePageState extends State<MyHomePage> {
+  late GraphNode<FamilyNode> root;
+  final Axis _direction = Axis.horizontal;
+  final bool _centerLayout = false;
+
+  final List<GraphNode<FamilyNode>> nodes = [
+    GraphNode<FamilyNode>(data: FamilyNode(name: 'Node 1'), isRoot: true),
+    GraphNode<FamilyNode>(data: FamilyNode(name: 'Node 2'), isRoot: true),
+    GraphNode<FamilyNode>(data: FamilyNode(name: 'Node 3'), isRoot: true),
+  ];
+
+  @override
+  void initState() {
+    root = nodes[0];
+    nodes[0].addNext(nodes[1]);
+    nodes[1].addNext(nodes[2]);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flow Graph'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.add),
+              onPressed: () {
+                final node = GraphNode<FamilyNode>(
+                  data: FamilyNode(name: 'Node ${nodes.length + 1}'),
+                );
+                nodes.add(node);
+                root.addNext(node);
+                // print(nodes.length);
+                setState(() {});
+              },
+            ),
+          ],
+        ),
+        // https://stackoverflow.com/questions/63297070/how-to-do-a-background-grid-color
+        body: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints) {
+            final double width = constraints.maxWidth;
+            final double height = constraints.maxHeight;
+            final h = Container(
+              width: 2,
+              height: height,
+              color: Colors.grey.withOpacity(0.2),
+            );
+            final v = Container(
+              width: width,
+              height: 2,
+              color: Colors.grey.withOpacity(0.2),
+            );
+            return Stack(
+              children: <Widget>[
+                ...List.generate(
+                  (width / 60).round(),
+                  (index) => Positioned(left: index * 60, child: h),
+                ),
+                ...List.generate(
+                  (height / 60).round(),
+                  (index) => Positioned(top: index * 60, child: v),
+                ),
+                DraggableFlowGraphView(
+                  root: root,
+                  direction: _direction,
+                  centerLayout: _centerLayout,
+                  willConnect: (node) => true,
+                  willAccept: (node) => true,
+                  builder: (context, node) {
+                    return CustomNodeWidget(
+                      node: node,
+                      onDelete: () => {
+                        node.deleteSelf(),
+                        setState(() {}),
+                        nodes.remove(node),
+                        //nodes.length,
+                      },
+                    );
+                  },
+                ),
+              ],
+            );
+          },
         ),
       ),
     );
   }
 }
 
-class FlowEditor extends StatefulWidget {
-  @override
-  _FlowEditorState createState() => _FlowEditorState();
+class FamilyNode {
+  FamilyNode({
+    required this.name,
+    this.singleChild = true,
+    this.multiParent = false,
+  });
+
+  String name;
+  bool singleChild;
+  bool multiParent;
 }
 
-class _FlowEditorState extends State<FlowEditor> {
-  List<Node> nodes = [];
-  Offset canvasOffset = Offset(0, 0);
-  int nodeIdCounter = 1;
+class CustomNodeWidget extends StatelessWidget {
+  final GraphNode<FamilyNode> node;
+  final Function onDelete;
 
-  void updateNodePosition(int nodeId, double dx, double dy) {
-    setState(() {
-      var node = nodes.firstWhere((node) => node.id == nodeId);
-      node.x += dx;
-      node.y += dy;
-    });
-  }
+  const CustomNodeWidget({
+    super.key,
+    required this.node,
+    required this.onDelete,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
+    return Container(
+      width: 100,
+      height: 100,
+      decoration: BoxDecoration(
+        color: Colors.red,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          // Draw the canvas
-          GestureDetector(
-            onPanUpdate: (details) {
-              setState(() {
-                canvasOffset += details.delta;
-              });
-            },
-            child: Container(
-              color: Colors.grey[200],
-            ),
+          Text(
+            node.data!.name,
+            style: const TextStyle(color: Colors.white, fontSize: 18),
           ),
-          // Draw the nodes and connectors
-          ...nodes.map((node) {
-            node.canvasOffset = canvasOffset;
-            node.updatePosition = (dx, dy) => updateNodePosition(node.id, dx, dy);
-            return node.buildWidget(context);
-          }),
+          PopupMenuButton(
+            itemBuilder: (context) {
+              return [
+                PopupMenuItem(
+                  child: const Text('Delete'),
+                  onTap: () {
+                    onDelete();
+                  },
+                )
+              ];
+            },
+          ),
         ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            nodes.add(Node(
-              x: canvasOffset.dx + 100,
-              y: canvasOffset.dy + 100,
-              id: nodeIdCounter++,
-              canvasOffset: canvasOffset,
-              updatePosition: (dx, dy) => updateNodePosition(nodeIdCounter, dx, dy),
-            ));
-          });
-        },
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-}
-
-class Connector {
-  double x;
-  double y;
-  String label;
-
-  Connector({
-    this.x = 0,
-    this.y = 0,
-    this.label = 'Connector',
-  });
-
-  Widget buildWidget(BuildContext context) {
-    return Container(
-      width: 50,
-      height: 25,
-      color: Colors.grey[200],
-      child: Text(label),
     );
   }
 }
